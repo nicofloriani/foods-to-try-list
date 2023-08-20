3a5c34d26356bdcaf8d7d4e57ff1fa36b0121760
nico_floriani <floriani.nico@gmail.com>
2023-08-20 11:38:33 -0700

    Added state to both App and NewFoodInput components to update UI and add item to list

--- src/NewFoodInput.js
+++ src/NewFoodInput.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NewFoodInput = () => {
+const NewFoodInput = (props) => {
+  const [food, setFood] = useState("");
+
+  const updateFood = (e) => {
+    setFood(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addFoodItem(food);
+    setFood("");
+  };
+
   return (
-    <form className="food-form">
-      <input type="text" />
+    <form className="food-form" onSubmit={handleSubmit}>
+      <input type="text" value={food} onChange={updateFood} />
       <button>Add to the list!</button>
     </form>
   );
