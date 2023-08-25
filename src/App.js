276616fa22dabe1e377744c0c723c79114e30bb1
nico_floriani <floriani.nico@gmail.com>
2023-08-25 11:46:01 -0700

    Created food component, function to delete food, event handler to delete food, and prevented empty list items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import NewFoodInput from "./NewFoodInput";
+import Food from "./Food";
 import "./styles.css";
 
 export default function App() {
@@ -9,13 +10,18 @@ export default function App() {
     setFoodsToTry([...foodsToTry, food]);
   };
 
+  const deleteFood = (foodToDelete) => {
+    const updatedFood = foodsToTry.filter((food) => food !== foodToDelete);
+    setFoodsToTry(updatedFood);
+  };
+
   return (
     <div className="App">
       <h1>New Foods to Try</h1>
       <NewFoodInput addFoodItem={addFoodItem} />
       <ul className="food-list">
         {foodsToTry.map((food, index) => (
-          <li key={index}>{food}</li>
+          <Food key={index} food={food} deleteFood={deleteFood} />
         ))}
       </ul>
     </div>
